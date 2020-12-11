6631fa0ad824432ba00fd9236f793c03af96a942
truptigaonkar <trups11.trupti@gmail.com>
2020-12-11 16:37:42 +0100

    Add project files.

---
+++ BackendPractice/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BackendPractice.MvcApplication" Language="C#" %>
---
+++ BackendPractice/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BackendPractice.Startup))]
+namespace BackendPractice
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
